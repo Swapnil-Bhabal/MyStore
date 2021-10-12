793cf512b03aa178db3edf624d318b34a6a9800e
Swapnil Bhabal <bhabalswaanimit19@student.mes.ac.in>
2021-10-12 12:07:23 +0530

    Step 4: Created product,rating and HomeScreen.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,23 @@
+import { Flex } from '@chakra-ui/react';
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import HomeScreen from './screens/HomeScreen';
 
 const App = () => {
     return (
-        <div>
+        <>
         <Header/>
+        <Flex as="main" 
+        direction="column"
+        mt="72px"
+        minH="xl"
+        py="6"
+        px="6"
+        bgColor="gray.200">
+        <HomeScreen/>
+        </Flex>
         <Footer/>
-        </div>
+        </>
     );
 };
 
