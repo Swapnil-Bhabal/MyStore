4b8ba630ebf555f37ce55dddfadbfc3424cdbad2
Swapnil Bhabal <bhabalswaanimit19@student.mes.ac.in>
2021-10-12 11:32:47 +0530

    Step 3: Created Header and Footer components.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,11 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
 const App = () => {
     return (
         <div>
-        <h1>MY STORE</h1>
+        <Header/>
+        <Footer/>
         </div>
     );
 };
