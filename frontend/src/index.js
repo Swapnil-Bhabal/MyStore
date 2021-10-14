319c4a7c043d7aafa373b131e8e7fcc08ee1b834
Swapnil Bhabal <bhabalswaanimit19@student.mes.ac.in>
2021-10-14 16:05:00 +0530

    Step 20 Completed: Redux installation and setup

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM  from 'react-dom';
 import { ChakraProvider } from '@chakra-ui/react';
+import { Provider } from 'react-redux';
+import store from './store';
 import App from './App';
 
 ReactDOM.render(
-    <React.StrictMode>
+    <Provider store={store}>
     <ChakraProvider>
     <App/>
     </ChakraProvider>
-    </React.StrictMode>,document.querySelector('#root')
+    </Provider>,document.querySelector('#root')
 );
