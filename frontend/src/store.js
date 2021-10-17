b51c9e1d7a0ab0f674bdafaf82df716de1d3ffb2
Swapnil Bhabal <bhabalswaanimit19@student.mes.ac.in>
2021-10-17 12:50:25 +0530

    Step 35 Completed: User login reducer and action

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -4,19 +4,28 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 
 import { productListReducer, productDetailsReducer } from './reducers/productReducer';
 import { cartReducer } from './reducers/cartReducer';
+import { userLoginReducer } from './reducers/userReducer';
 
 
 const reducer = combineReducers({
     productList: productListReducer,
     productDetails: productDetailsReducer,
     cart: cartReducer,
+    userLogin: userLoginReducer,
 });
 
 const cartItemsFromStorage = localStorage.getItem('cartItems')
     ? JSON.parse(localStorage.getItem('cartItems'))
     : [];
 
-const initialState = { cart: { cartItems: cartItemsFromStorage } };
+const userInfoFromStorage = localStorage.getItem('userInfo')
+    ? JSON.parse(localStorage.getItem('userInfo'))
+    : {};
+
+    const initialState = { 
+        cart: { cartItems: cartItemsFromStorage },
+        userLogin: { userInfo: userInfoFromStorage }, 
+    };
 
 const middlewares = [thunk];
 
