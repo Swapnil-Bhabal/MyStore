df97af83d3dee1af5222e97f550609b8e6f901bb
Swapnil Bhabal <bhabalswaanimit19@student.mes.ac.in>
2021-10-19 19:05:49 +0530

    Step 52 Completed: Backend endpoint for updating an order to paid.

--- backend/controller/orderController.js
+++ backend/controller/orderController.js
@@ -46,4 +46,26 @@ const getOrderById = asyncHandler(async (req, res) => {
     }
 });
 
-export { addOrderItems, getOrderById };
+const updateOrderToPaid = asyncHandler(async (req, red) => {
+    const order = await Order.findById(req.params.id);
+
+    if (order) {
+        order.isPaid = true;
+        order.paidAt = Date.now();
+        order.paymentResult = {
+            id: req.body.id,
+            status: req.body.status,
+            update_time: req.body.update_time,
+            email_address: req.body.payer.email_address,
+        };
+
+        const updatedOrder = await order.save();
+
+        res.json(updatedOrder);
+    } else {
+        res.status(404);
+        throw new Error('Order mot found');
+    }
+});
+
+export { addOrderItems, getOrderById, updateOrderToPaid };
--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { addOrderItems, getOrderById } from '../controller/orderController.js';
+import { addOrderItems, getOrderById, updateOrderToPaid } from '../controller/orderController.js';
 import { protect } from '../middleware/authMiddleware.js';
 
 
@@ -7,5 +7,6 @@ const router = express.Router();
 
 router.route('/').post(protect, addOrderItems);
 router.route('/:id').get(protect, getOrderById);
+router.route('/:id/pay').put(protect, updateOrderToPaid);
 
 export default router;
