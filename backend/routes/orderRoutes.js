d9a8cff0ec26a5f4d52de140e4645f73e4d270bf
Swapnil Bhabal <bhabalswaanimit19@student.mes.ac.in>
2021-10-19 16:40:10 +0530

    Step 49 completed: Get Order by ID(Backend Endpoint).

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { addOrderItems } from '../controller/orderController.js';
+import { addOrderItems, getOrderById } from '../controller/orderController.js';
 import { protect } from '../middleware/authMiddleware.js';
 import router from './productRoutes.js';
 
 const router = express.Router();
 
 router.route('/').post(protect, addOrderItems);
+router.route('/:id').get(protect, getOrderById);
 
 export default router;
