eced14449e205d2fc6bc56ebc12d7024493ac5a8
Swapnil Bhabal <bhabalswaanimit19@student.mes.ac.in>
2021-10-24 12:05:03 +0530

    Step 57 Completed: Admin Middleware and Getting Users Endpoint.

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -93,4 +93,9 @@ const registerUser = asyncHandler(async (req, res) => {
     }
 });
 
-export { authUser, getUserProfile, updateUserProfile, registerUser };
+const getUsers = asyncHandler(async (req, res) => {
+    const users = await User.find({});
+    res.json(users);
+});
+
+export { authUser, getUserProfile, updateUserProfile, registerUser, getUsers };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,11 +1,11 @@
 import express from 'express';
 
-import { protect } from '../middleware/authMiddleware.js'
-import { authUser, getUserProfile, registerUser, updateUserProfile } from '../controller/userController.js';
+import { protect, admin } from '../middleware/authMiddleware.js'
+import { authUser, getUserProfile, getUsers, registerUser, updateUserProfile } from '../controller/userController.js';
 
 const router = express.Router();
 
-router.route('/').post(registerUser);
+router.route('/').post(registerUser).get(protect, admin, getUsers);
 router.post('/login', authUser);
 router
     .route('/profile')
