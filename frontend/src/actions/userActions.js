966dbf485f3a40d07da13a58f01d70ddcdf00731
Swapnil Bhabal <bhabalswaanimit19@student.mes.ac.in>
2021-10-28 14:06:13 +0530

    Checked all the code and tested perfectly the final bug free edition of code.

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -84,7 +84,7 @@ export const register = (name, email, password) => async (dispatch) => {
             config
         );
 
-        dispatch({ tyoe: USER_REGISTER_SUCCESS, payload: data });
+        dispatch({ type: USER_REGISTER_SUCCESS, payload: data });
 
         dispatch({ type: USER_LOGIN_SUCCESS, payload: data });
 
